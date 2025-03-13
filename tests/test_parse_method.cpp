ce2731b705bc3dd5ce27e2fedda40fe573f9ca82
Callcold1 <kevin.mooney@hotmail.com>
2025-03-14 00:01:07 +0100

    added more tests

--- tests/test_parse_method.cpp
+++ tests/test_parse_method.cpp
@@ -11,43 +11,35 @@ void test_parse_method(std::string& str, const std::string& methodStr, int num, 
 	assert(req.getMethodString() == methodStr);
 	if (test == true){
 		assert(req.getResponseCode() == 200);
-		std::cout << "test_parse_http_request " << num << " passed!" << std::endl;
+		std::cout << "test_parse_method " << num << " passed : method valid\t: " << methodStr << std::endl;
 	}
 	else {
 		assert(req.getResponseCode() == 400);
 		assert(req.getLastErrorLoc() == "REQUEST LINE : METHOD");
-		std::cout << "test_parse_http_request " << num << " passed!" << std::endl;
+		std::cout << "test_parse_method " << num << " passed : method invalid\t: " << methodStr << std::endl;
 	}
 }
 
 int main() {
-	std::string	request0 = "GET https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request1 = "GEt https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request2 = "GeT https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request3 = "gET https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request4 = "geT https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request5 = "GEt https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request6 = "geT https://user@example.%63om/path HTTP/1.1\r\n";
-
-	std::string	request7 = "POST https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request8 = "Post https://user@example.%63om/path HTTP/1.1\r\n";
-	
-	std::string	request9 = "DELETE https://user@example.%63om/path HTTP/1.1\r\n";
-	std::string	request10 = "Delete https://user@example.%63om/path HTTP/1.1\r\n";
+	std::string	request0 = "GET https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request1 = "GEt https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request2 = "GeT https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request3 = "POST https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request4 = "Post https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request5 = "DELETE https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request6 = "Delete https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request7 = "MADEUP https://user@example.com/path HTTP/1.1\r\n";
+	std::string	request8 = "PUT https://user@example.com/path HTTP/1.1\r\n";
 
     test_parse_method(request0, "GET", 0, true);
     test_parse_method(request1, "GEt", 1, false);
     test_parse_method(request2, "GeT", 2, false);
-    test_parse_method(request3, "gET", 3, false);
-    test_parse_method(request4, "geT", 4, false);
-    test_parse_method(request5, "GEt", 5, false);
-    test_parse_method(request6, "geT", 6, false);
-
-    test_parse_method(request7, "POST", 7, true);
-    test_parse_method(request8, "Post", 8, false);
-    
-	test_parse_method(request9, "DELETE", 9, true);
-    test_parse_method(request10, "Delete", 10, false);
+    test_parse_method(request3, "POST", 3, true);
+    test_parse_method(request4, "Post", 4, false);
+	test_parse_method(request5, "DELETE", 5, true);
+    test_parse_method(request6, "Delete", 6, false);
+    test_parse_method(request7, "MADEUP", 7, false);
+    test_parse_method(request8, "PUT", 8, false);
 
     return 0;
 }
