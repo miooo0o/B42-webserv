725f2c9218f439e79156f833c0d75857f09976fd
Mina Kim <mmmiiinn@me.com>
2025-06-23 11:14:10 +0200

    chore(config): restructure fallback terminology to inherited values

--- Config/ServerConfig.cpp
+++ Config/ServerConfig.cpp
@@ -1,1 +1,43 @@
-#include "ServerConfig.hPP"
+#include "ServerConfig.hpp"
+
+ServerConfig::ServerConfig() : ConfigUnit() { applyInheritedValuesFrom(*this); }
+
+void	ServerConfig::addServerName(const std::string& name) {
+	_serverNames.push_back(name);
+
+}
+const std::vector<std::string>& ServerConfig::getServerNames() const {
+	return _serverNames;
+}
+
+void	ServerConfig::addLocation(Location loc) {
+	if (!loc.getUri().empty()) {
+		_locations[loc.getUri()] = loc;
+	}
+}
+
+const LocationsMap& ServerConfig::getLocations() const {
+	return _locations;
+}
+
+const Location* ServerConfig::findLocation(const std::string& uri) const {
+	std::map<std::string, Location>::const_iterator it = _locations.find(uri);
+	return it != _locations.end() ? &it->second : NULL;
+}
+
+void	ServerConfig::applyInheritedValuesFrom(const ConfigUnit& parent) {
+	ConfigUnit::applyInheritedValuesFrom(parent);
+
+	const ServerConfig* parent_unit = dynamic_cast<const ServerConfig*>(&parent);
+	if (parent_unit) {
+		if (!hasOverride(HAS_LISTEN))			_listen = parent_unit->getListen();
+		if (!hasOverride(HAS_ERROR_PAGES))	_errorPages = parent_unit->getErrorPages();
+	}
+}
+
+ServerConfig*	ServerConfig::baseDefaults() {
+	static ServerConfig instance(ConfigUnit::baseDefaults());
+	instance.setListen(ListenAddress());
+
+	return &instance;
+}
--- Config/ServerConfig.hpp
+++ Config/ServerConfig.hpp
@@ -2,29 +2,71 @@
  #define SERVERCONFIG_HPP
 
 #include <map>
-#include "ServerConfigBase.hpp"
+#include <string>
+#include <vector>
 
-class Location;
-class MimeMap;
-class ConfigInit;
+#include "ConfigCommon.hpp"
+#include "ConfigParser.hpp"
+#include "ConfigUnit.hpp"
+#include "Location.hpp"
 
-class ServerConfig : public ServerConfigBase {
+enum AddressFamily {
+	NON_AF_SET,
+	AF_IPV4,
+	AF_IPV6
+};
+
+struct ListenAddress {
+	AddressFamily	family;
+	std::string		host;	// e.g., "0.0.0.0" or "::"
+	uint16_t		port;	// e.g., 8080
+
+	ListenAddress() : family(NON_AF_SET), host(ConfigDefaults::LISTEN_HOST), port(ConfigDefaults::LISTEN_PORT) {}
+};
+
+typedef std::map<int, std::string>		ErrorPagesMap;
+typedef std::map<std::string, Location>	LocationsMap;
+
+class ServerConfig : public ConfigUnit {
+private:
+	ListenAddress					_listen;				// [REQUIRED]
+	std::vector<std::string>		_serverNames;
+	LocationsMap					_locations;
+	ErrorPagesMap					_errorPages;
+
+	bool							_chunkedSupport;		// default: true
+	bool							_serverTokens;			// show version info in header (default: false)
+	bool							_keepAlive;				// default: true
+	size_t							_keepAliveTimeout;		// default: 10s
+
+
 public:
-	explicit ServerConfig(const ConfigInit& init);
-	~ServerConfig();
+	ServerConfig();
+	ServerConfig(const ConfigUnit *parent);
+	virtual ~ServerConfig() {}
 
- 	const Location*	findLocation(const std::string& uri) const;
+	virtual void applyInheritedValuesFrom(const ConfigUnit& parent);	// overrride
+
+	// static, base defaults set
+	static ServerConfig*		baseDefaults();
+
+	CONFIG_OVERRIDE_ACCESSOR(listen, Listen, ListenAddress, HAS_LISTEN, ServerConfig)
+	CONFIG_OVERRIDE_ACCESSOR(errorPages, ErrorPages, ErrorPagesMap, HAS_ERROR_PAGES, ServerConfig)
+
+
+	// add
+	void							addServerName(const std::string& name);
+	void							addLocation(Location loc);
+
+	// find
+	const Location*					findLocation(const std::string& uri) const;
+
+	// get
+	const std::vector<std::string>&	getServerNames() const;
+	const LocationsMap&				getLocations() const;
+
+	// static
 
 };
 
-#endif //SERVERCONFIG_HPP
-
-/**
- * vector<config> <- host + port or ListenAddress struct  /server_name
- * content type
- * - json
- * - application/octet-stream
- * - x-www-form-urlendcoded
- * - formdata
- *
- */
+#endif // SERVERCONFIG_HP
